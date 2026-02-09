3d2c075b39709e2737a120ae7fca106e27c2c2c9
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-09 11:27:20 +0800

    Modify to add dark

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -1,21 +1,19 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
-
 import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useTheme } from '@/context/theme-context';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+  colorName: keyof typeof Colors.light
 ) {
-  const theme = useColorScheme() ?? 'light';
-  const colorFromProps = props[theme];
+  const { isDarkMode } = useTheme();
 
-  if (colorFromProps) {
-    return colorFromProps;
-  } else {
-    return Colors[theme][colorName];
+  if (isDarkMode && props.dark) {
+    return props.dark;
   }
+
+  if (!isDarkMode && props.light) {
+    return props.light;
+  }
+
+  return isDarkMode ? Colors.dark[colorName] : Colors.light[colorName];
 }
