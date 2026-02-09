8b91240ae091455fd01143d0abfdde48edc92fb4
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-09 11:25:01 +0800

    Modify the design of layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,16 +3,22 @@ import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Colors, LIGHT_BORDER, DARK_BORDER, LIGHT_BACKGROUND, DARK_BACKGROUND } from '@/constants/theme';
+import { useTheme } from '@/context/theme-context';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const { isDarkMode } = useTheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[isDarkMode ? 'dark' : 'light'].tint,
+        tabBarInactiveTintColor: Colors[isDarkMode ? 'dark' : 'light'].icon,
+        tabBarStyle: {
+          backgroundColor: isDarkMode ? DARK_BACKGROUND : LIGHT_BACKGROUND,
+          borderTopWidth: 1,
+          borderTopColor: isDarkMode ? DARK_BORDER : LIGHT_BORDER,
+        },
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
@@ -20,14 +26,35 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="about"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'About',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="info" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="notification"
+        options={{
+          title: 'Notification',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="bell" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gear" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="me"
+        options={{
+          title: 'Me',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person" color={color} />,
         }}
       />
     </Tabs>
